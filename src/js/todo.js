3884c0508b7e8d4aae6736a23bd08378f43fc4df
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-03-11 12:13:30 +0200

    task4

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { refs } from './refs';
 import storage from './storage';
 
@@ -12,13 +13,13 @@ function onInputValue(e) {
   const { name, value } = e.target;
 
   todo[name] = value;
-
+  todo.id = uuidv4();
   storage.save(LOCAL_STORAGE_KEY, todo);
 }
 
 function initTodo() {
   const savedTodo = storage.load(LOCAL_STORAGE_KEY);
-  
+
   if (savedTodo) {
     todo = savedTodo;
 
@@ -33,13 +34,15 @@ refs.form.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
   e.preventDefault();
 
-  if (todo.text === "" || todo.priority === "") {
-    alert("Заповніть усі поля!")
+  if (todo.text === '' || todo.priority === '') {
+    alert('Заповніть усі поля!');
     return;
   }
 
   console.log(todo);
+  const markup = `<li>todo:${todo.text}, priority ${todo.priority}</li>`;
+  refs.list.insertAdjacentHTML('afterbegin', markup);
   refs.form.reset();
   storage.remove(LOCAL_STORAGE_KEY);
-  Object.keys(todo).forEach(key => todo[key] = "");
+  Object.keys(todo).forEach(key => (todo[key] = ''));
 }
