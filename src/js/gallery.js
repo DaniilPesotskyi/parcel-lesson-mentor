d9fb6d16ebdcd46897b3f928df2063ad8cb53c9f
AShvab <shvabovska@gmail.com>
2023-03-25 12:27:07 +0200

    task4

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -14,6 +14,22 @@ const options = {
   itemsPerPage: 9,
   visiblePages: 5,
   page: 1,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+      '</a>'
+  }
 };
 
 const pagination = new Pagination(container, options);
@@ -26,7 +42,7 @@ service.getPopularImages(page).then(({ results, total }) => {
   pagination.reset(total);
   const markup = createGalleryCard(results);
   galleryList.insertAdjacentHTML('beforeend', markup);
-});
+}).catch(() => Notify.failure('Server error'));
 
 pagination.on('afterMove', getPopular);
 
@@ -55,6 +71,10 @@ function searchOnSubmit(e) {
   pagination.off('afterMove', getBySearch);
 
   service.getImagesBySearch(page).then(({ results, total }) => {
+    if (results.length ===0){
+      Notify.failure('Enter valid search query');
+      return;
+    }
     pagination.reset(total);
     galleryList.innerHTML = '';
     const markup = createGalleryCard(results);
